31aedab23303894ca3c35674abe11ec446d31192
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-12-06 00:06:08 +0300

    Change Base Url, delete shared view model on single activity for more South of Truth, update lib version

--- app/src/main/java/com/tech/satelittes/MainActivity.kt
+++ app/src/main/java/com/tech/satelittes/MainActivity.kt
@@ -2,9 +2,7 @@ package com.tech.satelittes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.navigation.NavController
-import com.tech.presentation.SatellitesListViewModel
 import com.tech.satelittes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,14 +11,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private val viewModel: SatellitesListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        viewModel.loadSatellites()
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- core/src/main/java/com/tech/core/Constants.kt
+++ core/src/main/java/com/tech/core/Constants.kt
@@ -3,6 +3,6 @@ package com.tech.core
 class Constants {
     companion object {
         const val BASE_URL =
-            "https://github.com/basaransuleyman//suleyman-basaranoglu-json/blob/main/"
+            "https://raw.githubusercontent.com/"
     }
 }
