c8be3cc97f707ab2f72830a77da37cc2e78e93d7
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-12-06 00:08:08 +0300

    Create Data Module with DI modules ; Use Case, Network , Coroutines

---
+++ core/src/main/java/com/tech/core/di/CoroutineModule.kt
@@ -0,0 +1,26 @@
+package com.tech.core.di
+
+import com.tech.core.IOCoroutineScope
+import com.tech.core.IODispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class CoroutineModule {
+
+    @Provides
+    @IOCoroutineScope
+    fun provideIOScope(
+        @IODispatcher dispatcher: CoroutineContext
+    ): CoroutineScope = CoroutineScope(dispatcher)
+
+    @Provides
+    @IODispatcher
+    fun provideIODispatcher(): CoroutineContext = Dispatchers.IO
+}
