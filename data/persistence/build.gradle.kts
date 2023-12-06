34ab41938f911655af4932ef8c4574721d75bc48
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-12-06 18:48:49 +0300

    Delete unused imports

--- data/persistence/build.gradle.kts
+++ data/persistence/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation(project(":domain"))
     implementation(project(":core"))
 
-    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.hilt.core)
     kapt(libs.hilt.compiler)
 
